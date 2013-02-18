d2aae23effa10c3d110db6f8a50ec6b4fa30bff1
Dmitry Malyk <dmitry.malyk@gmail.com>
2013-02-18 17:59:40 +0200

    18/02/2013

--- 1_chapter/03.rkt
+++ 1_chapter/03.rkt
@@ -16,3 +16,11 @@
 ;25
 ;25
 ;29
+
+(define (procedure2 x y z)
+(if(< x y) (if (< x z)(sum-of-squares y z)(sum-of-squares x y))(if(< y z)(sum-of-squares x z)(sum-of-squares x y))
+)
+(procedure2 5 5 2)
+(procedure2 2 3 4)
+(procedure2 3 2 4)
+(procedure2 -5 -5 -2)
